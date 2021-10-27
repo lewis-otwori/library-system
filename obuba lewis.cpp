e70af362df2424eff4661a56543c7f65c1ab420a
lewis-otwori <90901584+lewis-otwori@users.noreply.github.com>
2021-10-27 12:58:23 +0300

    Add files via upload

---
+++ obuba lewis.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+/*
+project:library system m
+author:lewis obuba
+date:oct 2021
+language:c99
+compiler:GCC
+license:MIT
+
+*/
+printf("hello!!! this is my library system project\n");
+printf("tcounty library system\n");
+printf("welcome lewis\n");
+printf("what would you like to do\n");
+printf("1.add a new user\n");
+printf("2.get a new book\n");
+printf("3.borrow a book\n");
+printf("4.access the internet\n");
+printf("5.write reports\n");
+printf("6.do research\n");
+printf("7.return a book\n");
+
+	
+	
+
+	
+	
+	
+	
+	return 0;
+	
+	
+}
